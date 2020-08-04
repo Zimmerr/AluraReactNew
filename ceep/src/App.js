e4b1f369b785a4d1749d9ffb8332a5d882a080b9
Luis Henrique <lhzimmermann@gmail.com>
2020-08-04 10:27:35 -0300

    Parte 2 - Aula 3
    Options dinamicas, separação entre dados e visualização

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -16,15 +16,15 @@ class App extends Component {
     };
   }
 
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto};
+  criarNota(titulo, texto, categoria){
+    const novaNota = {titulo, texto, categoria};
     const novoArrayNotas = [...this.state.notas,novaNota]
     const novoEstado = {
       notas:novoArrayNotas
     }
     this.setState(novoEstado)
   }
-  
+
   adicionarCategoria(nomeCategoria){
     const novoArrayCategorias = [...this.state.categorias, nomeCategoria]
     const novoEstado = {...this.state, categorias:novoArrayCategorias};
@@ -40,7 +40,10 @@ class App extends Component {
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <FormularioCadastro
+          categorias={this.state.categorias}
+          criarNota={this.criarNota.bind(this)}
+        />
         <main className="conteudo-principal">
           <ListaDeCategorias
             adicionarCategoria={this.adicionarCategoria.bind(this)}
