18759cb0624c43bf7f3734588d578ef79b6859ad
Luis Henrique <lhzimmermann@gmail.com>
2020-08-06 11:22:30 -0300

    Parte 3 - Aula 1
    Function components e MaterialUI

---
+++ form-cadastro/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro'
+
+function App() {
+  return (
+    <>
+      <h1>Formulario Cadastro</h1>
+      <FormularioCadastro/>
+    </>
+    
+  );
+}
+
+export default App;
