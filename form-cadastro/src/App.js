d8f8afae10773a3242ecace08610a6102e499ae2
Luis Henrique <lhzimmermann@gmail.com>
2020-08-07 11:27:23 -0300

    Parte 3 - Aula 5
    Validação de erros com state e props

--- form-cadastro/src/App.js
+++ form-cadastro/src/App.js
@@ -9,9 +9,17 @@ class App extends Component {
     return (
       <Container component="article" maxWidth="sm">
         <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
-        <FormularioCadastro aoEnviar={aoEnviarForm} />
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF}/>
       </Container>
     );
+  }
+}
+
+function validarCPF(cpf){
+  if(cpf.length !== 11){
+    return {valido:false, texto:"CPF deve ter 11 digitos."}
+  }else{
+    return {valido:true, texto:""}
   }
 }
 
