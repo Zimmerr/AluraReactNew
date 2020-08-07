200fc03b2473cf484728393c13e3b178b31b0a0b
Luis Henrique <lhzimmermann@gmail.com>
2020-08-07 11:19:30 -0300

    Parte 3 - Aula 4
    Regras dos hooks, state nas function components, desconstrução de objetos

--- form-cadastro/src/App.js
+++ form-cadastro/src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro'
 import 'fontsource-roboto';
 import {Container, Typography } from "@material-ui/core"
 
-function App() {
-  return (
-    <Container component="article" maxWidth="sm">
-      <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
-      <FormularioCadastro />
-    </Container>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
+        <FormularioCadastro aoEnviar={aoEnviarForm} />
+      </Container>
+    );
+  }
+}
+
+function aoEnviarForm(dados){
+  console.log(dados);
 }
 
 export default App;
