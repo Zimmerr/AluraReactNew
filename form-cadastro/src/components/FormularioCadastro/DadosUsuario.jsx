9f9cb7872ec19b118499250d7fe20db690711d55
Luis Henrique <lhzimmermann@gmail.com>
2020-08-11 12:10:12 -0300

    Parte 4 - Aula 4
    Contexto no React (useContext) e resolvendo prop drilling

--- form-cadastro/src/components/FormularioCadastro/DadosUsuario.jsx
+++ form-cadastro/src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,14 +1,37 @@
-import React, {useState} from "react";
+import React, { useState, useContext } from "react";
 import { TextField, Button } from "@material-ui/core";
+import ValidacoesCadastro from '../../contexts/validacoesCadastro'
 
-function DadosUsuario({aoEnviar}) {
+function DadosUsuario({ aoEnviar }) {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+
+  const [erros, setErros] = useState({ senha: { valido: true, texto: "" } });
+
+  const validacoes = useContext(ValidacoesCadastro);
+  function validarCampos(event) {
+    const { name, value } = event.target;
+    const novoEstado = { ...erros };
+    novoEstado[name] = validacoes[name](value);
+    setErros(novoEstado);
+  }
+
+  function possoEnviar() {
+    for (let campo in erros) {
+      if (!erros[campo].valido) {
+        return false;
+      }
+    }
+    return true;
+  }
   return (
-    <form onSubmit= {(event) => {
-      event.preventDefault();
-      aoEnviar({ email, senha });
-    }}
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (possoEnviar()) {
+          aoEnviar({ email, senha });
+        }
+      }}
     >
       <TextField
         value={email}
@@ -16,6 +39,7 @@ function DadosUsuario({aoEnviar}) {
           setEmail(event.target.value);
         }}
         id="email"
+        name="email"
         label="email"
         type="email"
         required
@@ -28,7 +52,11 @@ function DadosUsuario({aoEnviar}) {
         onChange={(event) => {
           setSenha(event.target.value);
         }}
+        onBlur={validarCampos}
+        error={!erros.senha.valido}
+        helperText={erros.senha.texto}
         id="senha"
+        name="senha"
         label="senha"
         type="password"
         required
@@ -37,7 +65,7 @@ function DadosUsuario({aoEnviar}) {
         fullWidth
       />
       <Button type="submit" variant="contained" color="primary">
-        Cadastrar
+        Próximo
       </Button>
     </form>
   );
